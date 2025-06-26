c55b052b939944e86a5ff592e5b6170e3480550a
Sean Adrien I. Regindin <sregindin@gmail.com>
2025-06-26 22:44:07 +0800

    Add files via upload

--- MP.py
+++ MP.py
@@ -43,5 +43,44 @@ buttonG.place(x=241, y=336)
 buttonH = tk.Button(root, text="H", command=lambda: print("Button clicked"))
 buttonH.place(x=285, y=520)
 
+buttonI = tk.Button(root, text="I", command=lambda: print("Button clicked"))
+buttonI.place(x=170, y=345)
+
+buttonJ = tk.Button(root, text="J", command=lambda: print("Button clicked"))
+buttonJ.place(x=112, y=375)
+
+buttonK = tk.Button(root, text="K", command=lambda: print("Button clicked"))
+buttonK.place(x=57, y=355)
+
+buttonL = tk.Button(root, text="L", command=lambda: print("Button clicked"))
+buttonL.place(x=213, y=485)
+
+buttonM = tk.Button(root, text="M", command=lambda: print("Button clicked"))
+buttonM.place(x=67, y=189)
+
+buttonN = tk.Button(root, text="N", command=lambda: print("Button clicked"))
+buttonN.place(x=137, y=189)
+
+buttonO = tk.Button(root, text="O", command=lambda: print("Button clicked"))
+buttonO.place(x=343, y=191)
+
+buttonP = tk.Button(root, text="P", command=lambda: print("Button clicked"))
+buttonP.place(x=484, y=201)
+
+buttonQ = tk.Button(root, text="Q", command=lambda: print("Button clicked"))
+buttonQ.place(x=524, y=201)
+
+buttonR = tk.Button(root, text="R", command=lambda: print("Button clicked"))
+buttonR.place(x=631, y=211)
+
+buttonS = tk.Button(root, text="S", command=lambda: print("Button clicked"))
+buttonS.place(x=481, y=150)
+
+buttonT = tk.Button(root, text="T", command=lambda: print("Button clicked"))
+buttonT.place(x=780, y=170)
+
+buttonU = tk.Button(root, text="U", command=lambda: print("Button clicked"))
+buttonU.place(x=10, y=420)
+
 # Start the GUI loop
 root.mainloop()
